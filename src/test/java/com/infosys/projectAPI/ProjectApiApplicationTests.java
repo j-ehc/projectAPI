e0d1f4d7ca4e399e4dad22341222cdad5f3e5d5a
Jesse <g00gleverse@gmail.com>
2020-11-12 20:55:05 -0800

    updated

--- src/test/java/com/infosys/projectAPI/ProjectApiApplicationTests.java
+++ src/test/java/com/infosys/projectAPI/ProjectApiApplicationTests.java
@@ -1,13 +1,42 @@
-package com.infosys.projectAPI;
+import org.junit.Assert;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjectApiApplicationTests {
+public class ProjectApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void basicTest() {
+		try {
+			Class.forName("com.infosys.projectAPI.ProjectApiApplication");
+		} catch(ClassNotFoundException e) {
+			Assert.fail("Should create a class called 'ProjectApiApplication'.");
+		}
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+//package com.infosys.projectAPI;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//
+//class ProjectApiApplicationTests {
+//
+//	@Test
+//	public void basicTest(){
+//
+//		ProjectApiApplication test = new ProjectApiApplication();
+//		assertEquals
+//
+//	}
+//
+//}
